19e13ad2c46ff08dd5b6724918f5b923efd1d73f
Ted Han <ted@knowtheory.net>
2018-11-26 11:27:59 -0500

    Add some parsing so we can just get raw text.

--- azure/azure.rb
+++ azure/azure.rb
@@ -30,8 +30,19 @@ def ocr(path)
 
   dirname = File.dirname(path)
   basename = File.basename(path, ".*")
-  #File.open("#{dirname}/#{basename}.azure.txt", 'w'){ |f| f.puts data.text }
+  File.open("#{dirname}/#{basename}.azure.txt", 'w'){ |f| f.puts parse_results(response.body) }
   File.open("#{dirname}/#{basename}.azure.json", 'w'){ |f| f.puts response.body }
+end
+
+def parse_results(data)
+  # keys: ["language", "textAngle", "orientation", "regions"]
+  data["regions"].map do |region|
+    region["lines"].map do |line|
+      line["words"].map do |word|
+        word["text"]
+      end.join(" ")
+    end.join("\n")
+  end
 end
 
 arg_path = ARGV.first
@@ -47,5 +58,14 @@ paths.each do |path|
   if wait_time > 0
     puts  "sleeping for #{wait_time} seconds"
     sleep wait_time
+  end
+end
+
+def output_text(paths)
+  paths.each do |path|
+    dirname = File.dirname(path)
+    basename = File.basename(path, ".*")
+    data = JSON.parse(File.read(path))
+    File.open("#{dirname}/#{basename}.txt", 'w'){ |f| f.puts parse_results(data) }
   end
 end
