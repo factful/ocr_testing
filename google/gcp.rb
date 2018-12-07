5d39bd83152258435c203dbbacfb4517c9b722a8
Ted Han <ted@knowtheory.net>
2018-12-07 13:41:43 -0800

    Some comments.

--- google/gcp.rb
+++ google/gcp.rb
@@ -1,4 +1,6 @@
-#export GOOGLE_APPLICATION_CREDENTIALS=credentials.json
+# run the following to give google's library
+# the credentials it needs to function:
+# export GOOGLE_APPLICATION_CREDENTIALS=credentials.json
 
 # Imports the Google Cloud client library
 require "google/cloud/vision"
@@ -12,24 +14,34 @@ vision = Google::Cloud::Vision.new project: project_id
 # The name of the image file to annotate
 arg_path = ARGV.first
 
+# Get a list of all of the pngs and/or jpgs to OCR
 File.directory?(arg_path)
 paths = Dir.glob(File.join arg_path, '*.{png,jpg}')
 
 puts "Annotating #{paths.count} images..."
 
+# create a bucket that we're going to push results into.
 annotations = []
+# grab 16 images at a time into a batch (google will take up to 16 in a single batch)
 paths.each_slice(16) do |image_paths|
+  # open up the images,
   images = image_paths.map{ |f| vision.image(f) }
+  # and send the batch to Google for OCR.
   results = vision.annotate do |annotator| 
     images.each do |i| 
       annotator.annotate(i, text:true)
     end
   end
+  # then stick this batch of results in our bucket
   annotations.push *results
 end
 
+# Zip the results up with the paths (they should both be in the same order)
 annotation_map = Hash[paths.zip(annotations)]
 
+# grab the path to the original file name, and the data extracted for it
+# then write just the text out to a file (with the same name as the image)
+# and then dump all of the data (including position information) into another file.
 annotation_map.each do |path, data|
   dirname = File.dirname(path)
   basename = File.basename(path, ".*")
--- tesseract/ocr.rb
+++ tesseract/ocr.rb
@@ -1,10 +1,15 @@
 #! /usr/bin/env ruby
 
+# Tesseract makes things easy.
+# Just say `tesseract [image to ocr'd] [destination that it'll suffix with .txt]`
 def ocr(target, destination)
   cmd = "tesseract #{target} #{destination}.tesseract"
   `#{cmd}`
 end
 
+# In order to OCR a PDF, it must first be turned into images.
+# mutool makes that pretty easy, and we're just going to default to
+# exporting PNGs at 300 dpi.
 def convert(target, destination)
   resolution = true ? "-O resolution=300" : ""
   cmd = "mutool convert #{resolution} -F png -o #{destination} #{target}"
@@ -13,6 +18,7 @@ end
 
 require 'fileutils'
 
+# Get a path to be OCR'd and check if it exists.
 filename = ARGV.first
 if filename.nil? or filename.empty?
   # puts help info & exit
@@ -35,6 +41,7 @@ else
   [filename]
 end
 
+# Then OCR all the things in the queue.
 ocr_queue.each_with_index do |path, index|
   puts "OCRing page #{index+1} of #{ocr_queue.size}"
   ocr(path, "#{File.dirname(path)}/#{File.basename(path, ".*")}")
