2996162b3046f0f0b41c25ce6708c7d59bc5c3d6
Ted Han <ted@knowtheory.net>
2018-12-12 13:50:20 -0800

    Annotating the Azure code a little.

--- azure/azure.rb
+++ azure/azure.rb
@@ -9,31 +9,49 @@
 #  * Image file size must be less than 4MB.
 #  * Image dimensions must be at least 50 x 50.
 
+# Azure doesn't have an API library for Ruby (or much 
+# of anything from what i can tell), so instead we're 
+# going to use RestClient to interact with their REST API directly.
 require 'rest-client'
 require 'json'
 
 def ocr(path)
+  # We're just defaulting to the East US region.
   region = "eastus"
+  # This is the REST endpoint we're going to communicate with.
   url = "https://#{region}.api.cognitive.microsoft.com/vision/v2.0/ocr"
+  # Azure's other demos indicate that we can specify 
+  # the language or set it to "unk".  We'll also have
+  # it guess at the orientation, just in case our
+  # documents are oriented in a direction other than up.
   payload = {
     data: File.open(path, 'rb'),
     language: 'unk',
     detectOrientation: 'true'
   }
+  # In order to make the API actually accept a request
+  # we'll need to read the credentials somewhere.
+  # I'm reading it out of the ENV.
   headers = {
     'Ocp-Apim-Subscription-Key': ENV['AZURE_KEY'],
     'Content-Type': 'application/json'
   }
   puts "OCRing #{path}"
+  # Send the request to Azure
   response = RestClient.post(url, payload, headers)
   data = JSON.parse(response.body)
 
   dirname = File.dirname(path)
   basename = File.basename(path, ".*")
+  # Extract the text out of the response and write it into a file.
   File.open("#{dirname}/#{basename}.azure.txt", 'w'){ |f| f.puts parse_results(response.body) }
+  # Write the raw JSON data out into a file.
   File.open("#{dirname}/#{basename}.azure.json", 'w'){ |f| f.puts response.body }
 end
 
+# Azure doesn't provide the text in an easily accesible way.
+# In order to get the text, you have to unpeel the position
+# data/containers to get down to the individual words.
 def parse_results(data)
   # keys: ["language", "textAngle", "orientation", "regions"]
   data["regions"].map do |region|
@@ -45,6 +63,7 @@ def parse_results(data)
   end
 end
 
+# Read the first 
 arg_path = ARGV.first
 File.directory?(arg_path)
 paths = Dir.glob(File.join arg_path, '*.{png,jpg}')
