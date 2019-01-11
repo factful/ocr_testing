85b14a46dc71865c0bcdc90c9176f3b970c3d34b
Ted Han <ted@knowtheory.net>
2019-01-11 15:33:58 -0800

    roughed in Svelte viewer.

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -1,13 +1,19 @@
 import App from './App.html';
 
 import contents from './contents.js'
+let entries = Object.entries(contents).map( (row)=>{ return row; } )
 
 const app = new App({
 	target: document.body,
 	data: {
+		count: 0,
 		name: 'world',
-		pages: Object.entries(contents).map( (row)=>{ return row; } )
+		pages: entries,
+		activePage: entries[0][0]
+	},
+	methods: {
 	}
 });
+
 
 export default app;
