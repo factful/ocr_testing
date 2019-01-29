51d2046f11ee533d964a841218140bdc9de5dc62
Amanda on Rajje <amanda@factful.io>
2019-01-29 15:19:03 -0800

    Light wordsmithing.

--- lib/ocr/acrobat.rb
+++ lib/ocr/acrobat.rb
@@ -1,5 +1,5 @@
 =begin
-  Since Acrobat is a desktop app, there's not really a way to
+  Acrobat is a desktop app -- there's not really a way to
   manipulate it from the commandline.  That said, we do need
   to be able to extract the text out of these PDFs programmatically
   We can shell out to qpdf, or use docsplit.
@@ -7,6 +7,6 @@
 
 module OCR
   class Acrobat
-    
+
   end
 end
